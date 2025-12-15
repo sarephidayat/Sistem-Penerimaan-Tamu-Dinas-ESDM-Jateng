fde8d44db1c77443119870a0559815b87fdc1451
MUHAMMAD SYARIFUDIN HIDAYAT <pekokdika97@gmail.com>
2025-12-15 15:00:42 +0700

    none

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,6 @@ use App\Http\Controllers\CheckoutController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PemesananController;
 use App\Http\Controllers\StatistikController;
-use App\Http\Controllers\ProfileController;
 
 
 // ===============================Index================================
@@ -158,5 +157,3 @@ Route::get('/kepala-dinas', [KadisController::class, 'index'])
     ->name('kepala-dinas');
 
 
-Route::get('/api/chart-tren-kunjungan', [ChartController::class, 'GrafikTrenKunjungan']);
-Route::get('/chart-agenda-mingguan', [ChartController::class, 'chartAgendaMingguan']);
